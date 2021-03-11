b0c3e94d28ac4430c96a59a54014dd3a716428ed
hyeongcheolkim <tori1753@naver.com>
2021-03-11 17:17:12 +0900

    week2

--- uos_prgintro/week2/week2_02.py
+++ uos_prgintro/week2/week2_02.py
@@ -1,5 +1,5 @@
 # Univeristy of Seoul. Dept of Computer Science.
 # 2018430021
-# week2_01
+# week2_02
 
 print("+=============+\n|    김형철   |\n+=============+ ")
--- uos_prgintro/week2/week2_03.py
+++ uos_prgintro/week2/week2_03.py
@@ -1,5 +1,5 @@
 # Univeristy of Seoul. Dept of Computer Science.
 # 2018430021
-# week2_01
+# week2_03
 
 print("1+2+3을 계산하면",1+2+3,"입니다.")
